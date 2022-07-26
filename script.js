540ba35a3b3911a59cbef585f1473e128e7d9649
Squashim <konrad18v1@wp.pl>
2022-07-26 15:14:23 +0200

    30% done

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const bill = document.getElementById("bill");
-const tipLabels = document.querySelectorAll(".tips label");
-const tip = document.querySelectorAll('input[name = "tip"]');
-const customTip = document.querySelector(".tips input[number]");
-
-for (let i = 0; i < tipLabels.length; i++) {
-	customTip.addEventListener("onentry");
-}
+const tipBtns = document.querySelectorAll('.tips input[type="radio"]');
+console.log(tipBtns);
+tipBtns.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		console.log(btn);
+	});
+});
