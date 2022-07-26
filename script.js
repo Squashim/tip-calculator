703201439b464fa4d58f27049674e03a623e8856
Squashim <konrad18v1@wp.pl>
2022-07-26 12:07:40 +0200

    Second commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 const bill = document.getElementById("bill");
+const tipLabels = document.querySelectorAll(".tips label");
+const tip = document.querySelectorAll('input[name = "tip"]');
+const customTip = document.querySelector(".tips input[number]");
+
+for (let i = 0; i < tipLabels.length; i++) {
+	customTip.addEventListener("onentry");
+}
