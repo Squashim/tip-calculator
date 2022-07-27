31d6721818fb6e50e8d7a9d5ec8816388eef6f3f
Squashim <konrad18v1@wp.pl>
2022-07-27 17:09:05 +0200

    65% done

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 const tipBtns = Array.from(
 	document.querySelectorAll('.tips input[type="radio"]')
 );
+const customTip = document.querySelector("#custom");
+const people = document.querySelector("#people");
+const tipAmount = document.querySelector(".tip-amount");
+const total = document.querySelector(".total");
+const resetBtn = document.querySelector(".reset");
+
+function checkError(value, id) {
+	let num;
+	const errors = document.querySelectorAll(".error");
+	if (id === "bill") {
+		num = 0;
+	} else if (id === "people") {
+		num = 1;
+	}
+	if (value === "" || value <= 0) {
+		errors[num].style.display = "block";
+	} else {
+		errors[num].style.display = "none";
+		return;
+	}
+}
+
+function checkCustomTip(value) {
+	const billError = document.querySelector(".error-bill");
+	if (value === "" || value <= 0) {
+		billError.style.display = "block";
+	} else {
+		billError.style.display = "none";
+		return;
+	}
+}
 
 for (const btn of tipBtns) {
 	btn.addEventListener("click", () => {
