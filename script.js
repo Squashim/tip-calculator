7d1d7366008bbde47602e74ba294cb9c02a0adf5
Squashim <konrad18v1@wp.pl>
2022-07-27 17:43:37 +0200

    70% done

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const tipBtns = Array.from(
 	document.querySelectorAll('.tips input[type="radio"]')
 );
+const bill = document.querySelector("#bill");
 const customTip = document.querySelector("#custom");
 const people = document.querySelector("#people");
 const tipAmount = document.querySelector(".tip-amount");
@@ -8,27 +9,43 @@ const total = document.querySelector(".total");
 const resetBtn = document.querySelector(".reset");
 
 function checkError(value, id) {
-	let num;
 	const errors = document.querySelectorAll(".error");
-	if (id === "bill") {
-		num = 0;
-	} else if (id === "people") {
-		num = 1;
+	let num;
+	switch (id) {
+		case "bill":
+			num = 0;
+			break;
+		case "people":
+			num = 1;
+			break;
 	}
+
 	if (value === "" || value <= 0) {
 		errors[num].style.display = "block";
+		if (num === 0) {
+			bill.style.borderColor = "indianred";
+		} else {
+			people.style.borderColor = "indianred";
+		}
 	} else {
 		errors[num].style.display = "none";
+		if (num === 0) {
+			bill.style.borderColor = "hsl(183, 100%, 15%)";
+		} else {
+			people.style.borderColor = "hsl(183, 100%, 15%)";
+		}
 		return;
 	}
 }
 
 function checkCustomTip(value) {
-	const billError = document.querySelector(".error-bill");
-	if (value === "" || value <= 0) {
-		billError.style.display = "block";
+	if (value <= 0 || value > 100) {
+		customTip.value = "";
+		customTip.style.setProperty("--r", "indianred");
+		customTip.style.borderColor = "indianred";
+		customTip.placeholder = "0-100%";
 	} else {
-		billError.style.display = "none";
+		customTip.style.borderColor = "hsl(183, 100%, 15%)";
 		return;
 	}
 }
